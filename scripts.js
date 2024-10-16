a470bab632f28f5cc47ab9204271e81b13d9893a
Ljubomir Josifovski <ljubomir@gigul2>
2024-10-16 13:45:03 +0100

    better pic toggles, add garden office

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 // File: scripts.js
-function showImage() {
-    var img = document.getElementById('zoomableImage');
+
+// Show image
+function showImage(imageId) {
+    var img = document.getElementById(imageId);
     img.style.display = 'block'; // Show the image
 }
 
+// Show or hide image, depending on the current state
+function toggleShowImage(imageId) {
+    var img = document.getElementById(imageId);
+    if (img.style.display === 'none' || img.style.display === '') {
+        // Show the image
+        img.style.display = 'block';
+    } else {
+        // Hide the image
+        img.style.display = 'none';
+    }
+}
+
+// Zoom or unzoom image, depending on the current state
 function zoomImage(img) {
     // Check the current width to toggle between states
     if (img.style.width === '100%' || img.style.width === '') { 
